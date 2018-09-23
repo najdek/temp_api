46099b3244a503c3cd84717a7000c530b0196e1c
Mateusz Najdek <mateusz@najdek.me>
2018-09-23 11:10:32 +0200

    get: last day/month/6months/year, with hourly/daily averages

--- get.php
+++ get.php
@@ -15,15 +15,33 @@ exit();
 }
 
 if (isset($_GET['last'])) {
-  if (($_GET['last'] > 0) && ($_GET['last'] < 100) && (ctype_digit($_GET['last']))) {
+  if (($_GET['last'] > 0) && ($_GET['last'] < 10100) && (ctype_digit($_GET['last']))) {
     $sql = "SELECT * FROM room_" . $room . " ORDER BY datetime DESC LIMIT " . $_GET['last'];
   } else {
     $sql = "SELECT * FROM room_" . $room . " ORDER BY datetime DESC LIMIT 1";
   }
 } else {
-  if (isset($_GET['lastday'])) {
-    $datetimeto = date('Y-m-d H:i:s');
-    $datetimefrom = date("Y-m-d H:i:s", strtotime("-24 hours"));
+  if (isset($_GET['lastx'])) {
+    if ($_GET['lastx'] == "day") {
+      $datetimeto = date('Y-m-d H:i:s');
+      $datetimefrom = date("Y-m-d H:i:s", strtotime("-24 hours"));
+    }
+    if ($_GET['lastx'] == "month") {
+      $datetimeto = date('Y-m-d H:i:s');
+      $datetimefrom = date("Y-m-d H:i:s", strtotime("-1 month"));
+      $average = "hour";
+    }
+    if ($_GET['lastx'] == "6months") {
+      $datetimeto = date('Y-m-d H:i:s');
+      $datetimefrom = date("Y-m-d H:i:s", strtotime("-6 months"));
+      $average = "day";
+    }
+    if ($_GET['lastx'] == "year") {
+      $datetimeto = date('Y-m-d H:i:s');
+      $datetimefrom = date("Y-m-d H:i:s", strtotime("-1 year"));
+      $average = "day";
+    }
+
   } else {
     $datetimefrom = $_GET['from'];  //format "2018.09.08-18:26:00"
     $datetimeto = $_GET['to'];
@@ -59,9 +77,27 @@ if($sql_result = mysqli_query($link, $sql)) {
   while ($sql_row = $sql_result->fetch_assoc()) {
     $output[$sql_row['datetime']] = $sql_row['temperature'];
   }
-  $output = json_encode($output);
+  if ($average == "hour" || $average == "day") {
+    $outputaverage = array();
+    foreach ($output as $datetime => $temperature) {
+      if ($average == "day") {
+        $date = explode(" ", $datetime)[0];
+        $outputaverage[$date][] = $temperature;
+      }
+      if ($average == "hour") {
+        $date = explode(":", $datetime)[0];
+        $outputaverage[$date . ":00"][] = $temperature;
+      }
+    }
+    foreach ($outputaverage as $date => &$temperatures) {
+      $temperatures = array_sum($temperatures)/count($temperatures);
+      $temperatures = round($temperatures, 2);
+    }
+    $output = json_encode($outputaverage);
+  } else {
+    $output = json_encode($output);
+  }
   echo $output;
-
 } else {
   echo "ERROR: " . mysqli_error($link);
 }
